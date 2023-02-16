30acf2b196951768d592acf547cb405c435fcea2
KyungaIM <avocadopajamas@gmail.com>
2023-02-16 16:21:40 +0900

    feat:fetct api

--- client/src/pages/QuestionDetail.js
+++ client/src/pages/QuestionDetail.js
@@ -1,4 +1,14 @@
-const QuestionDetail = () => {
+import useFetch from '../util/useFetch';
 
+const QuestionDetail = () => {
+    const [todos, isPending, error] = useFetch(`${process.env.REACT_APP_API_URL}/status`);
+    return(
+        <div >
+        {/* { isPending && <Loading />}
+        { todos && 
+        <p>{todos}</p>} */}
+        안녕
+        </div>
+    )
 }
 export default QuestionDetail

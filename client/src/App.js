675964f5515d90e3fa8f64ef2ff81401bbc6bbd8
JudiPARK0416 <p.eunjung.0425@gmail.com>
2023-02-20 17:17:11 +0900

    프론트 병합중

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, Suspense ,useState } from "react";
+import { useEffect, Suspense, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 import Login from "./pages/Login";
@@ -17,57 +17,74 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  const [login,setLogin] = useState(false);
-  const [userInfo,setUserInfo] = useState(null);
-  const [error,setError] = useState(null)
-  //QuestionList -> QuestionDetail로 엔드포인트를 전달합니다
-  const [endpoint, setEndpoint] = useState(null);
+    const [login, setLogin] = useState(false);
+    const [userInfo, setUserInfo] = useState(null);
+    const [error, setError] = useState(null);
+    //QuestionList -> QuestionDetail로 엔드포인트를 전달합니다
+    const [endpoint, setEndpoint] = useState(null);
 
-  //로그인 여부를 확인해 회원정보를 저장합니다
-  useEffect(() => {
-    const abortCont = new AbortController();
+    //로그인 여부를 확인해 회원정보를 저장합니다
+    useEffect(() => {
+        const abortCont = new AbortController();
 
-    setTimeout(() => {
-      //여기서 endpoint를 수정해주면 됩니다
-    fetch(process.env.REACT_APP_API_URL, 
-          { signal: abortCont.signal,
-            //인증정보를 포함하는 옵션입니다
-            credentials: "include" 
-          })
-    .then(res => {
-        if (!res.ok) { 
-            throw Error('could not fetch the data for that resource');
-        } 
-        return res.json();
-    })
-    .then(data => {
-        setLogin(true);
-        setUserInfo(data);
-        setError(null);
-    })
-    .catch(err => {
-        setLogin(false);
-        setError(err.message);
-    })
-    }, 1000);
+        setTimeout(() => {
+            //여기서 endpoint를 수정해주면 됩니다
+            fetch(process.env.REACT_APP_API_URL, {
+                signal: abortCont.signal,
+                //인증정보를 포함하는 옵션입니다
+                credentials: "include",
+            })
+                .then((res) => {
+                    if (!res.ok) {
+                        throw Error(
+                            "could not fetch the data for that resource"
+                        );
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setLogin(true);
+                    setUserInfo(data);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setLogin(false);
+                    setError(err.message);
+                });
+        }, 1000);
 
-    return () => abortCont.abort();
-  }, []);
+        return () => abortCont.abort();
+    }, []);
 
-  return (
-    <BrowserRouter>
-    <Suspense>
-        <Routes>
-          <Route exact path="/" element={<QuestionList />} />
-          <Route path="/question-form" element={<QuestionForm />} />
-          <Route path="/question-detail" element={<QuestionDetail login={login} userInfo={userInfo} endpoint={endpoint}/>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-    </Suspense>
-    </BrowserRouter>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Suspense>
+                    <Routes>
+                        <Route exact path="/" element={<QuestionList />} />
+                        <Route
+                            path="/question-form"
+                            element={<QuestionForm />}
+                        />
+                        <Route
+                            path="/question-detail"
+                            element={
+                                <QuestionDetail
+                                    login={login}
+                                    userInfo={userInfo}
+                                    endpoint={endpoint}
+                                />
+                            }
+                        />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/sign-in" element={<SignIn />} />
+                        <Route path="/mypage" element={<MyPage />} />
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;

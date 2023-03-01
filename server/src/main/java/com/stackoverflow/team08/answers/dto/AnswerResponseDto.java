527e8f273c98c0f92d0548b1ab9820f9cb4a9789
lKori <yuy0841@gmail.com>
2023-03-01 21:50:17 +0900

    feat:answer question 매핑 수정

--- server/src/main/java/com/stackoverflow/team08/answers/dto/AnswerResponseDto.java
+++ server/src/main/java/com/stackoverflow/team08/answers/dto/AnswerResponseDto.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AnswerResponseDto {
     private long memberId;
+    private long questionId;
     private long answerId;
     private String content;
     private int voteCount;

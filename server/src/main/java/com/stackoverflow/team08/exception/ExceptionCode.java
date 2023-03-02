58e52ef2fe6b5789305f827369e7a74e4ec6aa36
kkw <kkte02@naver.com>
2023-03-02 11:35:14 +0900

    add headerconfig

--- server/src/main/java/com/stackoverflow/team08/exception/ExceptionCode.java
+++ server/src/main/java/com/stackoverflow/team08/exception/ExceptionCode.java
@@ -1,18 +1,20 @@
 package com.stackoverflow.team08.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 public enum ExceptionCode {
-    MEMBER_NOT_FOUND(404, "Member Not Found"),
+    MEMBER_NOT_FOUND(404, "존재하지 않는 이메일입니다."),
     MEMBER_EXISTS(409, "Member exists"),
     QUESTION_NOT_FOUND(404, "Question not found"),
     ANSWER_NOT_FOUND(404, "Answer not found"),
     TAG_NOT_FOUND(404, "Tag Not Found"),
     COMMENT_NOT_FOUND(404, "Comment Not Found"),
     VOTE_NOT_ALLOW (405, "You're already voted"),
-    MEMBER_SECESSION(404, "MEMBER_SECESSION"),
-    DISPLAY_NAME_EXISTS(404, "This DisplayName is exists"),
-    UNAUTHORIZED_MEMBER(401, "Unauthorized member");
+    MEMBER_SECESSION(404, "탈퇴한 회원입니다."),
+    DISPLAY_NAME_EXISTS(404, "이미 존재하는 닉네임 입니다."),
+    UNAUTHORIZED_MEMBER(401, "권한이 없는 사용자 입니다."),
+    BAD_CREDENTIAL(400, "비밀번호가 맞지 않습니다.");
     @Getter
     private int status;
 
